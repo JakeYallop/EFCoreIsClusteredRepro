0f3663fcc71fcccfe0e6772660c84b10e40f7fe0
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2021-09-26 19:23:21 +0100

    Initial commit

---
+++ EFCoreIsClusteredRepro/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+public class Context : DbContext
+{
+    public DbSet<MyObject> MyObjects { get; set; } = null!;
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Integrated Security=true");
+        base.OnConfiguring(optionsBuilder);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<MyObject>().HasKey(x => x.Id).IsClustered();
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
+
+public class MyObject
+{
+    public int Id { get; set; }
+    public string Property { get; set; } = null!;
+}
